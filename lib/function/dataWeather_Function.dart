ca780bc15f26cbf38aefdf386860a8c59e354ebb
muneeb-shahid <muneebshahid0102@gmail.com>
2023-02-25 22:47:48 +0500

    the new comit

--- lib/function/dataWeather_Function.dart
+++ lib/function/dataWeather_Function.dart
@@ -2,7 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:weatherapp/constants/Colors.dart';
 
 class Weather_Data {
-  static Data_Weather_text( String text, double fontsize) {
+  static data_Weather_Days(
+    String text,
+    double fontsize,
+    String text1,
+    assetName,
+    String text2,
+    context
+  ) {
+    return Padding(
+      padding: EdgeInsets.only(left: 10,top: 10),
+      child: Row(
+        children: [
+          Text(
+            text,
+            style: TextStyle(
+                color: App_Colors.app_white_color,
+                fontSize: fontsize,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+                letterSpacing: 2),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width*0.03,),
+          Icon(
+            Icons.sunny,
+            color: Colors.yellow,
+            size: 25,
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width*0.03,),
+    
+          Text(
+            text1,
+            style: TextStyle(
+                color: App_Colors.app_white_color,
+                fontSize: fontsize,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+                letterSpacing: 2),
+          ),
+          SizedBox(width: MediaQuery.of(context).size.width*0.03,),
+    
+          Image(image: AssetImage(assetName)),
+          SizedBox(width: MediaQuery.of(context).size.width*0.03,),
+    
+          Text(
+            text2,
+            style: TextStyle(
+                color: App_Colors.app_white_color,
+                fontSize: fontsize,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Poppins',
+                letterSpacing: 2),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Data_Weather_text(String text, double fontsize) {
     return Text(
       text,
       style: TextStyle(
@@ -12,8 +69,6 @@ class Weather_Data {
           fontFamily: 'Poppins',
           letterSpacing: 2),
     );
-
-   
   }
 
   static data_Wether_container_text(
@@ -30,7 +85,7 @@ class Weather_Data {
         Text(
           text,
           style: TextStyle(
-             fontWeight: FontWeight.w600,
+            fontWeight: FontWeight.w600,
             color: App_Colors.app_white_color,
             fontSize: 16,
             fontFamily: 'Poppins',
@@ -50,7 +105,7 @@ class Weather_Data {
         Text(
           text1,
           style: TextStyle(
-             fontWeight: FontWeight.w600,
+            fontWeight: FontWeight.w600,
             color: App_Colors.app_white_color,
             fontSize: 16,
             fontFamily: 'Poppins',
